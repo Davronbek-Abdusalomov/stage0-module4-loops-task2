0e6292a8679988501dd28b1b9766eed01b1c965d
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-02 15:50:20 +0500

    While

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int nums = 0;
+        while(nums<=multiplyByAndToInclusive){
+            System.out.println(multiplyByAndToInclusive*nums);
+            nums++;
+        }
     }
 }
