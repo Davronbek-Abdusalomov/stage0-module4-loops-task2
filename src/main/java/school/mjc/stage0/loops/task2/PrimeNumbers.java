11fdd4377c60051c9128c750ef347cecc3c97a48
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-03 16:45:09 +0500

    While

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int num = 2;
+        while(num<=printToInclusive){
+            for (int i=1; i<=num; i++) {
+                int count=0;
+                if (num%i==0){
+
+                }
+
+            }
+        }
     }
 }
