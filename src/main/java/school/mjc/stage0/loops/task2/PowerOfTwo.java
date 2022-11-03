12d08acf2ab4fe6bd267a412e7b5279bc2627548
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-11-03 16:54:43 +0500

    While

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int num = 0;
+        if (num>=0){
+            while (num<=power){
+                System.out.println(Math.pow(2, num));
+                num++;
+            }
+        }
+        else System.out.println("too much power");
 
     }
 }
